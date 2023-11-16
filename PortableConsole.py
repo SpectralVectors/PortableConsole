5e8c1c3e5456ad3a47b634ec4bdc12229455baca
SpectralVectors <robmuir1@gmail.com>
2023-11-16 11:20:45 -0500

    New UI, added print() and Run Script functions
    UI is now laid out in 3 separate boxes with props to enter or select the code, and an operator below that will execute it.
    The top box is the same as the previous addon, allowing you to enter and execute a line of code.
    The second box will print whatever you query to the System Console.
    The third box allows you to choose one of the text blocks in the blend file and run it from within whatever context you like.

--- PortableConsole.py
+++ PortableConsole.py
@@ -1,7 +1,9 @@
+import bpy
+
 bl_info = {
     "name": "PortableConsole",
     "author": "Spectral Vectors",
-    "version": (0, 0, 2),
+    "version": (0, 1, 0),
     "blender": (2, 90, 0),
     "location": "F3 > PortableConsole or Shift+Ctrl+Alt+P",
     "description": "A popup panel to run Python in any area or context",
@@ -10,35 +12,103 @@ bl_info = {
     "category": "Development",
 }
 
-import bpy
+
+class PortableConsoleProperties(bpy.types.PropertyGroup):
+
+    Command: bpy.props.StringProperty(
+        name='Command',
+        description='The Python command you wish to execute',
+        default='',
+    )
+
+    Print: bpy.props.StringProperty(
+        name='print()',
+        description='Prints the entered data to the console',
+        default='',
+    )
+
+    Script: bpy.props.PointerProperty(
+        type=bpy.types.Text
+    )
+
+
+class PortablePrintOperator(bpy.types.Operator):
+    """Prints Data to the System Console"""
+
+    bl_idname = "addon.portable_print"
+    bl_label = "Print to System Console"
+
+    def execute(self, context):
+        props = context.scene.portable_console_properties
+        exec(f"print({props.Print})")
+        return {'FINISHED'}
+
+
+class ExecuteCommandOperator(bpy.types.Operator):
+    """Execute Python commands in any context"""
+
+    bl_idname = "addon.execute_command"
+    bl_label = "Execute Command"
+
+    def execute(self, context):
+        props = context.scene.portable_console_properties
+        exec(props.Command)
+        return {'FINISHED'}
+
+
+class RunScriptOperator(bpy.types.Operator):
+    """Runs the selected script in the current context"""
+
+    bl_idname = "addon.run_script"
+    bl_label = "Run Script"
+
+    def execute(self, context):
+        props = context.scene.portable_console_properties
+        exec(props.Script.as_string())
+        return {'FINISHED'}
 
 
 class PortableConsoleOperator(bpy.types.Operator):
     """A popup panel that lets you execute Python commands in any context"""
 
     bl_idname = "addon.portable_console"
-    bl_label = "Execute Python Command"
-    bl_property = 'Command'
-
-    Command: bpy.props.StringProperty(
-        name='Command:',
-        description='The Python command you wish to execute',
-        default='',
-    )
+    bl_label = "Portable Python Console"
 
     def execute(self, context):
-        exec(self.Command)
         return {'FINISHED'}
 
     def invoke(self, context, event):
         return context.window_manager.invoke_props_dialog(self)
 
     def draw(self, context):
-        self.layout.prop(self, 'Command')
+        props = context.scene.portable_console_properties
+        layout = self.layout
+        box = layout.box()
+        column = box.column()
+        column.prop(props, 'Command')
+        column.operator('addon.execute_command')
+        box = layout.box()
+        column = box.column()
+        column.prop(props, 'Print')
+        column.operator('addon.portable_print')
+        box = layout.box()
+        column = box.column()
+        column.prop_search(props, 'Script', bpy.data, 'texts', text='Script')
+        column.operator('addon.run_script')
+
+
+classes = [
+    PortableConsoleProperties,
+    PortablePrintOperator,
+    ExecuteCommandOperator,
+    PortableConsoleOperator,
+    RunScriptOperator,
+]
 
 
 def register():
-    bpy.utils.register_class(PortableConsoleOperator)
+    for cls in classes:
+        bpy.utils.register_class(cls)
 
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.user
@@ -51,8 +121,12 @@ def register():
         shift=True,
         ctrl=True,
         alt=True,
-        )
+    )
     kmi.active = True
+
+    bpy.types.Scene.portable_console_properties = bpy.props.PointerProperty(
+        type=PortableConsoleProperties
+    )
 
 
 def unregister():
@@ -64,7 +138,8 @@ def unregister():
         if (key.idname == "addon.portable_console"):
             keymap.remove(key)
 
-    bpy.utils.unregister_class(PortableConsoleOperator)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
 
 
 if __name__ == "__main__":
